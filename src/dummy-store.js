654f54ca34f77a44b432f69a5136a16b88ed7ea6
Jordan Bohme <jordan.m.bohme@gmail.com>
2019-07-22 13:30:46 -0600

    added dummy-store

---
+++ src/dummy-store.js
@@ -0,0 +1,31 @@
+export default {
+    'users': [
+        {
+            'id': '',
+            'email': '',
+            'password': '',
+            'name': '',
+            'last_login': '',
+            'rank': '',
+        },
+    ],
+    'games': [
+        {
+            'id': '',
+            'name': '',
+            'description': '',
+            'players': '',
+            'time': '',
+            'image': '',
+
+        },
+    ],
+    'game_nights': [
+        {
+            'id': '',
+            'date': '',
+            'location': '',
+            'name': '',
+        },
+    ],
+}
